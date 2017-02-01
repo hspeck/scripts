2eb526898d44977a47af0d76e2d107bba0c1612d
hspeck <hspeck@ucla.edu>
2017-01-31 17:48:22 -0800

    made UltBabSearch.sh use variables and a for loop, not a while loop and holder file

--- UltBabSearch.sh
+++ UltBabSearch.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-tail -n +2 $1 | cut -f 1 |uniq > UltBabSearchHolderDoc.txt
+Individuals=$(tail -n +2 $1 | cut -f 1 | sort -n|uniq)
 
-while read ID 
+for ID in $Individuals
 	do printf "Individual: $ID: Times sampled: "	
 	tail -n +2 $1| grep -wcE "^$ID"
-done < UltBabSearchHolderDoc.txt
+done
--- netsize_all.sh
+++ netsize_all.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 for file in ~/Desktop/eeb-177/CSB/unix/data/Saavedra2013/*.txt
-	do printf "$file ";
-	cat $file | wc -l;
-	head -n +1 $file |grep -oE "[01] "| wc -l
-	done
+do 
+	row=$(cat $file | wc -l)
+	column=$(head -n +1 $file |grep -oE "[01] "| wc -l)
+	printf "$file $row $column \n"	
+done
